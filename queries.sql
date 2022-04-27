06c4dadb86db2ca579043cf6bc3c5bf3f4b9b6df
Sydney Friedel <sfriede5@jh.edu>
2022-04-27 10:51:24 -0400

    fixed queries

--- queries.sql
+++ queries.sql
@@ -220,7 +220,7 @@ ORDER BY Ed.avgTeacherStartingSalary DESC;
 
 -- 17. Ordering the states from least to most racially diverse, list the state GDP and median household income.
 -- https://archives.huduser.gov/healthycommunities/sites/default/files/public/Racial%20Diversity%20using%20Shannon-Wiener%20Index.pdf
--- link above used as a guide for calculating diversity index
+-- link above used as a guide for calculating diversity index (we scaled by 10000 since our data is on a different scale than the source)
 DELIMITER // 
 
 DROP FUNCTION IF EXISTS GetDiversityIndex //
@@ -228,23 +228,20 @@ DROP FUNCTION IF EXISTS GetDiversityIndex //
 CREATE FUNCTION GetDiversityIndex(stateName_param VARCHAR(15))
 RETURNS DECIMAL(5,3)
 BEGIN
-      WITH AggregateDiversity AS (SELECT stateName, AVG(D.white) AS white, AVG(D.black) AS black, AVG(D.asian) AS asian, AVG(D.indigenous) AS indigenous, AVG(D.other) AS other
-                                  FROM Demographics AS D
-                                 )
-      IF EXISTS (SELECT * FROM Demographics WHERE stateName = stateName_param) THEN
-         SELECT FORMAT(1 / (D.white * A.white + D.black * A.black + D.asian * A.asian + D.indigenous * A.indigenous + D.other * A.other), 3)
-         INTO @diversityIndex
-         FROM Demographics AS D, AggregateDiversity AS A
-         ON D.stateName = AggregateDiversity.stateName
-	      WHERE stateName = stateName_param;
-         RETURN @diversityIndex;
-      ELSE
-         RETURN 0;
-      END IF;
+	IF EXISTS (SELECT * FROM Demographics WHERE stateName = stateName_param) THEN
+      	   WITH AggregateDiversity AS (SELECT AVG(white) AS white, AVG(black) AS black, AVG(asian) AS asian, AVG(indigenous) AS indigenous, AVG(other) AS other FROM Demographics)
+           SELECT FORMAT(10000 / (D.white * A.white + D.black * A.black + D.asian * A.asian + D.indigenous * A.indigenous + D.other * A.other), 3)
+           INTO @diversityIndex
+           FROM Demographics AS D JOIN AggregateDiversity AS A
+	   WHERE D.stateName = stateName_param;
+           RETURN @diversityIndex;
+      	ELSE
+	   RETURN 0;
+        END IF;
 END; //
 
 DELIMITER ;
 
-SELECT GetDiversityIndex(stateName), realGDP, medianIncome
+SELECT stateName, GetDiversityIndex(stateName), realGDP, medianIncome
 FROM Economy
 ORDER BY GetDiversityIndex(stateName) ASC;
