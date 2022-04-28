1693df31f8a78a887486d5a2024e20d4f4aa8b2e
shelby <coe.shelby@gmail.com>
2022-04-28 10:26:28 -0400

    home page

--- queries.sql
+++ queries.sql
@@ -121,38 +121,7 @@ SELECT E.stateName, E.highschoolGradRate, E.avgSATScore, E.avgACTScore
 FROM Health AS H JOIN Education AS E ON H.stateName = E.stateName
 WHERE H.drugOverdoses >= (SELECT avgRateDrugs FROM AggregateStats) + 2* (SELECT stddevDrugs FROM AggregateStats) OR H.suicideRate >= (SELECT avgRateSuicides FROM AggregateStats) + 2*(SELECT stddevSuicides FROM AggregateStats);
 
-DELIMITER //
 
-DROP PROCEDURE IF EXISTS Query9 //
-
-CREATE PROCEDURE Query9
-BEGIN    
-   --  MODIFIED CODE FROM 4/19 CLASS
-   SET @sql = NULL;
-
-
-   -- concatenate the assignment name list and associated expressions
-   -- into a larger query string so we can execute it, but leave ?
-   -- in place so we can plug in the specific sid value in a careful way
-   
-
-   SET @sql = CONCAT('WITH AggregateStats AS (SELECT AVG(drugOverdoses) AS 'avgRateDrugs', 
-                     AVG(suicideRate) AS 'avgRateSuicides', STD(drugOverdoses) AS 'stddevDrugs', STD(suicideRate) AS 'stddevSuicides'
-                     FROM Health)
-                     SELECT E.stateName, E.highschoolGradRate, E.avgSATScore, E.avgACTScore
-                     FROM Health AS H JOIN Education AS E ON H.stateName = E.stateName
-                     WHERE H.drugOverdoses >= (SELECT avgRateDrugs FROM AggregateStats) + 2* (SELECT stddevDrugs FROM AggregateStats) OR H.suicideRate >= (SELECT avgRateSuicides FROM AggregateStats) + 2*(SELECT stddevSuicides FROM AggregateStats);');
-   -- alert the server we have a statement shell to set up
-   PREPARE stmt FROM @sql;
-
-   -- now execute the statement shell
-   EXECUTE stmt;
-
-   -- tear down the prepared shell since no longer needed (we won't requery it)
-   DEALLOCATE PREPARE stmt;
-END; //
-
-DELIMITER ;
 
 -- 10. In descending order of education spending per pupil, list each state's percentage of adults who have completed college and the highschool graduation rate.
 SELECT Education.percentCompletingCollege, Education.highschoolGradRate
---
+++ query9.sql
@@ -0,0 +1,35 @@
+-- Sydney Friedel and Shelby Coe
+-- sfriede5 and scoe4
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS Query9 //
+
+CREATE PROCEDURE Query9
+BEGIN    
+   --  MODIFIED CODE FROM 4/19 CLASS
+   SET @sql = NULL;
+
+
+   -- concatenate the assignment name list and associated expressions
+   -- into a larger query string so we can execute it, but leave ?
+   -- in place so we can plug in the specific sid value in a careful way
+   
+
+   SET @sql = CONCAT('WITH AggregateStats AS (SELECT AVG(drugOverdoses) AS 'avgRateDrugs', 
+                     AVG(suicideRate) AS 'avgRateSuicides', STD(drugOverdoses) AS 'stddevDrugs', STD(suicideRate) AS 'stddevSuicides'
+                     FROM Health)
+                     SELECT E.stateName, E.highschoolGradRate, E.avgSATScore, E.avgACTScore
+                     FROM Health AS H JOIN Education AS E ON H.stateName = E.stateName
+                     WHERE H.drugOverdoses >= (SELECT avgRateDrugs FROM AggregateStats) + 2* (SELECT stddevDrugs FROM AggregateStats) OR H.suicideRate >= (SELECT avgRateSuicides FROM AggregateStats) + 2*(SELECT stddevSuicides FROM AggregateStats);');
+   -- alert the server we have a statement shell to set up
+   PREPARE stmt FROM @sql;
+
+   -- now execute the statement shell
+   EXECUTE stmt;
+
+   -- tear down the prepared shell since no longer needed (we won't requery it)
+   DEALLOCATE PREPARE stmt;
+END; //
+
+DELIMITER ;
