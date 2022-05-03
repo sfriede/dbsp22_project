fad7d707936a783ae4ad85b6699d1d3f20d136cb
shelby <coe.shelby@gmail.com>
2022-05-03 13:33:50 -0400

    query 11

--- query11.sql
+++ query11.sql
@@ -14,7 +14,16 @@ BEGIN
         SET @sql = CONCAT('SELECT E.stateName, H.suicideRate, H.teenPregnancyRate 
                             FROM Economy AS E JOIN Health AS H ON E.stateName = H.stateName
                             WHERE E.stateName IN (SELECT * FROM HighestStates) OR E.stateName IN (SELECT * FROM LowestStates)
-                            ORDER BY ', '?', ' DESC;');
+                            ORDER BY ', '?', ' DESC');
+        
+        /* WITH HighestStates AS (SELECT stateName
+                            FROM Economy
+                            ORDER BY ', '?',' DESC
+                            LIMIT 10),
+                            LowestStates AS (SELECT stateName
+                            FROM Economy
+                            ORDER BY ', '?',' ASC
+                            LIMIT 10)*/
 
         -- alert the server we have a statement shell to set up
         PREPARE stmt FROM @sql;
