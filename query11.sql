dd825ccb9c2b7538098002992e76e04e1e2fc763
shelby <coe.shelby@gmail.com>
2022-05-03 14:35:08 -0400

    query 11

--- query11.sql
+++ query11.sql
@@ -10,18 +10,20 @@ DROP PROCEDURE IF EXISTS Query11 //
 
 CREATE PROCEDURE Query11(IN factor VARCHAR(30))
 BEGIN
-	WITH HighestStates AS (SELECT stateName
-    FROM Economy
-    ORDER BY factor DESC
-    LIMIT 10),
-    LowestStates AS (SELECT stateName
-    FROM Economy
-    ORDER BY factor ASC
-    LIMIT 10)
-    SELECT E.stateName, E.factor, H.suicideRate, H.teenPregnancyRate 
-    FROM Economy AS E JOIN Health AS H ON E.stateName = H.stateName
-    WHERE E.stateName IN (SELECT * FROM HighestStates) OR E.stateName IN (SELECT * FROM LowestStates)
-    ORDER BY E.factor DESC;
+    IF factor = 'readGDP'
+	    WITH HighestStates AS (SELECT stateName
+        FROM Economy
+        ORDER BY realGDP DESC
+        LIMIT 10),
+        LowestStates AS (SELECT stateName
+        FROM Economy
+        ORDER BY realGDP ASC
+        LIMIT 10)
+        SELECT E.stateName, E.realGDP, H.suicideRate, H.teenPregnancyRate 
+        FROM Economy AS E JOIN Health AS H ON E.stateName = H.stateName
+        WHERE E.stateName IN (SELECT * FROM HighestStates) OR E.stateName IN (SELECT * FROM LowestStates)
+        ORDER BY E.realGDP DESC;
+    END IF;
 
 END; //
 
