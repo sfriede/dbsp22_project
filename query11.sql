690a4e14f0c96caa954174ebe4c2684730792bc1
shelby <coe.shelby@gmail.com>
2022-05-03 14:27:57 -0400

    query 11

--- query11.sql
+++ query11.sql
@@ -1,40 +1,28 @@
 -- Sydney Friedel and Shelby Coe
 -- sfriede5 and scoe4
+-- query 6 (modified to take user input): For states with the highest and lowest median incomes, what is the difference between the US-born and foreign-born median incomes, and how racially diverse is this state? what does this look like for a state of the user's choice? 
 
 DELIMITER //
 
+-- get median income for states where teen pregnancy rate is higher than average
+
 DROP PROCEDURE IF EXISTS Query11 //
 
-CREATE PROCEDURE Query11(IN factor VARCHAR(20))
-BEGIN    
-    
-        --  MODIFIED CODE FROM 4/19 CLASS
-        SET @sql = NULL;
-   
-        SET @sql = CONCAT('SELECT E.stateName, H.suicideRate, H.teenPregnancyRate 
-                            FROM Economy AS E JOIN Health AS H ON E.stateName = H.stateName
-                            ORDER BY ' '?');
-        
-        /* WITH HighestStates AS (SELECT stateName
-                            FROM Economy
-                            ORDER BY ', '?',' DESC
-                            LIMIT 10),
-                            LowestStates AS (SELECT stateName
-                            FROM Economy
-                            ORDER BY ', '?',' ASC
-                            LIMIT 10)
-                            
-                            WHERE E.stateName IN (SELECT * FROM HighestStates) OR E.stateName IN (SELECT * FROM LowestStates)*/
+CREATE PROCEDURE Query11(IN factor VARCHAR(30))
+BEGIN
+	WITH HighestStates AS (SELECT stateName
+    FROM Economy
+    ORDER BY factor DESC
+    LIMIT 10),
+    LowestStates AS (SELECT stateName
+    FROM Economy
+    ORDER BY factor ASC
+    LIMIT 10)
+    SELECT E.stateName, factor, H.suicideRate, H.teenPregnancyRate 
+    FROM Economy AS E JOIN Health AS H ON E.stateName = H.stateName
+    WHERE E.stateName IN (SELECT * FROM HighestStates) OR E.stateName IN (SELECT * FROM LowestStates)
+    ORDER BY factor DESC;
 
-        -- alert the server we have a statement shell to set up
-        PREPARE stmt FROM @sql;
-
-         -- now execute the statement shell with a value plugged in for the ?
-        EXECUTE stmt USING factor;
-
-        -- tear down the prepared shell since no longer needed (we won't requery it)
-        DEALLOCATE PREPARE stmt;
-   
 END; //
 
 DELIMITER ;
