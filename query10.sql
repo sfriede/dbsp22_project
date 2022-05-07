6c973d16823fe1e30b2be601011b8470f40eb1ad
Sydney Friedel <sfriede5@jh.edu>
2022-05-07 15:19:03 -0400

    merging

--- query10.sql
+++ query10.sql
@@ -1,9 +1,8 @@
 -- Sydney Friedel and Shelby Coe
 -- sfriede5 and scoe4
 -- query 10: in descending order of education spending per pupil, see each states highschool grad rate and percent completing college
-DELIMITER //
 
--- get median income for states where teen pregnancy rate is higher than average
+DELIMITER //
 
 DROP PROCEDURE IF EXISTS Query10 //
 
